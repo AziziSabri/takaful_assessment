1bec7ea6b31c950553dacae9c89dd1bbce154312
AziziSabri <azizi.allisabri@gmail.com>
2024-11-29 21:40:32 +0800

    Initial commit

---
+++ src/main/java/com/example/takaful_assessment/TakafulAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.takaful_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakafulAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakafulAssessmentApplication.class, args);
+	}
+
+}
